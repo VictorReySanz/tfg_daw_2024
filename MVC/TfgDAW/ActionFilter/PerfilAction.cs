82d836c4ef7754dd5e9fcb79ff3023fa0442454a
luis ayllon <luis.ayllonherrera@educa.madrid.org>
2024-05-10 00:20:48 +0200

    cubo  carpeta actionfilter para el login

---
+++ MVC/TfgDAW/ActionFilter/PerfilAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TfgDAW.ActionFilter
+{
+    //[AttributeUsageAttribute(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class PerfilAction : ActionFilterAttribute
+    {
+        public bool Disable { get; set; }
+        public string Profiles { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Disable) return;
+            var sessionContents = filterContext.HttpContext.Session.Contents;
+
+            if (!this.Profiles.Contains(sessionContents["rol"].ToString()))
+            {
+                filterContext.Result = new RedirectResult("~/Home/Unauthorized");
+            }
+        }
+
+    }
+}
---
+++ MVC/TfgDAW/ActionFilter/SesionAction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TfgDAW.Models;
+
+namespace TfgDAW.ActionFilter
+{
+    public class SesionAction : ActionFilterAttribute
+    {
+        
+        private share_enjoyEntities db = new share_enjoyEntities();
+
+        public bool Disable { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Disable) return;
+            UpdateUserSession(filterContext);
+        }
+
+        private void UpdateUserSession(ActionExecutingContext filterContext)
+        {
+            var usuario = filterContext.HttpContext.User.Identity.Name.ToLower();
+            filterContext.HttpContext.Session.Clear();
+            filterContext.HttpContext.Session.Add("UserCorreo", usuario);
+            List<Usuarios> userObj = db.Usuarios.Where(u => u.email == usuario).ToList();
+            if (userObj.Count == 0)
+            {
+                filterContext.Result = new RedirectResult("~/Home/Forbidden");
+            }
+            else
+            {
+                Usuarios u = userObj.First();
+                filterContext.HttpContext.Session.Add("nombre", u.nombre);
+                filterContext.HttpContext.Session.Add("foto", u.foto);
+                filterContext.HttpContext.Session.Add("email", u.email);
+                filterContext.HttpContext.Session.Add("rol", u.rol_id);      
+                //u.Fecha = DateTime.Now;
+                db.Entry(u).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+    }
+}
