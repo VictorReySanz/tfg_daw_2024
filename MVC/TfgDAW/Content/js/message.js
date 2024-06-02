16fce0567ccf3c49f2a770de3771389d706e3218
luis ayllon <luis.ayllonherrera@educa.madrid.org>
2024-06-02 12:26:28 +0200

    .

--- MVC/TfgDAW/Content/js/message.js
+++ MVC/TfgDAW/Content/js/message.js
@@ -1,6 +1,10 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿
+document.addEventListener("DOMContentLoaded", function () {
     var messageBox = document.getElementById("messageBox");
     if (messageBox.textContent.trim() !== "") {
         messageBox.classList.add("show");
+        setTimeout(function () {
+            messageBox.classList.remove("show");
+        }, 4000); // Ocultar después de 4 segundos
     }
 });
